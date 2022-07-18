2ca0c3f60bfa6b7d68152b4d13b503a5bcf9419d
Gerald Versluis <gerald@verslu.is>
2022-07-18 10:02:58 +0200

    Initial commit

---
+++ MauixUnitTestSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauixUnitTestSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MauixUnitTestSample
+ Sample code to demonstrate how to add a xUnit test project to your .NET MAUI app
